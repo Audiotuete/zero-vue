25836612494d6c792b2fc352e717a38b92cd86bb
Audiotuete <jan.moll@protonmail.com>
2023-10-17 18:23:56 +0000

    create: Table component

---
+++ src/components/Tables/Table.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+type TableHeaders = {
+  key: string
+  name: string
+}
+
+const props = defineProps<Props>()
+interface Props {
+  data?: object
+  tableHeaders?: TableHeaders[]
+}
+
+function digForValue(row: any, keys: string[]): string {
+  if (keys.length > 1) {
+    return digForValue(row[keys.shift()!], keys)
+  } else {
+    return row[keys[0]]
+  }
+}
+</script>
+
+<template>
+  <table style="text-align: left">
+    <tr>
+      <th v-for="{ name } in props.tableHeaders">{{ name }}</th>
+    </tr>
+
+    <tr v-for="row in data" draggable="false" :ondragstart="() => {}" :ondragover="() => {}">
+      <td v-for="{ key } in props.tableHeaders">{{ digForValue(row, key.split('.')) }}</td>
+    </tr>
+  </table>
+</template>
+
+<style scoped></style>
