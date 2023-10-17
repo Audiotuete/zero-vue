40024f316bc53d1a7b302ca74ba99bd046d3aeda
Audiotuete <jan.moll@protonmail.com>
2023-10-17 18:28:32 +0000

    create: Link component

---
+++ src/components/Link.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<Props>()
+interface Props {
+  href: string
+}
+</script>
+
+<template>
+  <a class="link" :href="props.href">
+    <slot />
+  </a>
+</template>
+
+<style scoped>
+.link {
+  text-decoration: underline;
+}
+</style>
