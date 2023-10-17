41bd82a7efc9784b15ac148c9b46ab4dde0cdb3b
Audiotuete <jan.moll@protonmail.com>
2023-10-17 18:28:24 +0000

    create: Highlight component

---
+++ src/components/Highlight.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<Props>()
+interface Props {
+  bold?: boolean
+  italic?: boolean
+  underline?: boolean
+  strike?: boolean
+}
+const renderAs = computed(() => {
+  if (props.bold) {
+    return 'strong'
+  } else if (props.italic) {
+    return 'em'
+  } else if (props.strike) {
+    return 's'
+  } else {
+    return 'span'
+  }
+})
+</script>
+
+<template>
+  <component
+    :is="renderAs"
+    :class="{
+      underline: props.underline
+    }"
+  >
+    <slot />
+  </component>
+</template>
+
+<style scoped>
+.underline {
+  text-decoration: underline;
+}
+</style>
