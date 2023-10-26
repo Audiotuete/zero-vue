fbc0e51992ad4364fdbd406ed50a4375232f26f1
Audiotuete <jan.moll@protonmail.com>
2023-10-26 16:07:44 +0000

    fix: nested / disabled conflict & add: name prop

--- src/components/forms/CheckboxGroup.vue
+++ src/components/forms/CheckboxGroup.vue
@@ -1,46 +1,55 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 
-const props = defineProps<Props>()
-interface Props {
-  data: any[]
-  row?: string
-  title?: string
+export type Checkbox = {
+  name: string
+  label: string
+  checked?: boolean
+  disabled?: boolean
+  children?: Checkbox[] | null
 }
 
-const emit = defineEmits<{
-  'update:modelValue': [value: object]
-}>()
+const props = defineProps<Props>()
+interface Props {
+  name: string
+  description?: string
+  data: Checkbox[]
+}
 
-const optionsList = ref([])
+const optionsList = ref<HTMLLIElement[]>([])
 
 onMounted(() => {
   props.data.map((item, index) => {
     let listItem = optionsList.value[index]
 
-    let option = listItem.querySelector('input.option')
-    let subOptions = listItem.querySelectorAll('input.subOption')
+    let option: HTMLInputElement | null = listItem.querySelector('input.option')
+    let subOptions: NodeListOf<HTMLInputElement> = listItem.querySelectorAll('input.subOption')
 
-    option.onclick = function () {
-      if (subOptions.length) {
-        for (var i = 0; i < subOptions.length; i++) {
-          subOptions[i].checked = this.checked
-          item.children[i].checked = this.checked
+    option!.onclick = function () {
+      const el = <HTMLInputElement>this
+
+      if (item.children) {
+        for (let i = 0; i < subOptions.length; i++) {
+          if (!subOptions[i].disabled) {
+            subOptions[i].checked = el.checked
+            item.children[i].checked = el.checked
+          }
         }
       }
     }
 
     if (subOptions.length) {
       for (let i = 0; i < subOptions.length; ++i) {
-        console.log(subOptions[i])
-
         subOptions[i].onclick = function () {
-          var checkedCount = listItem.querySelectorAll('input.subOption:checked').length
+          let activeCount = listItem.querySelectorAll('input.subOption:not([disabled])').length
+          let checkedCount = listItem.querySelectorAll('input.subOption:checked').length
 
-          option.checked = checkedCount > 0
-          item.checked = checkedCount > 0
+          let cleanedCheckCount = checkedCount
 
-          option.indeterminate = checkedCount > 0 && checkedCount < subOptions.length
+          item.checked = cleanedCheckCount > 0
+          option!.checked = cleanedCheckCount > 0
+
+          option!.indeterminate = cleanedCheckCount > 0 && cleanedCheckCount < activeCount
         }
       }
     }
@@ -49,16 +58,29 @@ onMounted(() => {
 </script>
 
 <template>
+  <span v-if="props.description">{{ props.description }}</span>
   <ul ref="ul">
     <li v-for="box in data" ref="optionsList">
       <label>
-        <input v-model="box.checked" type="checkbox" class="option" />
+        <input
+          v-model="box.checked"
+          :name="box.name"
+          type="checkbox"
+          class="option"
+          :disabled="box.disabled"
+        />
         {{ box.label }}
       </label>
-      <ul v-if="box.children">
+      <ul v-if="box.children" class="nested">
         <li v-for="deepBox in box.children">
           <label>
-            <input v-model="deepBox.checked" type="checkbox" class="subOption" />
+            <input
+              :name="deepBox.name"
+              v-model="deepBox.checked"
+              type="checkbox"
+              class="subOption"
+              :disabled="deepBox.disabled || box.disabled"
+            />
             {{ deepBox.label }}
           </label>
         </li>
@@ -67,4 +89,9 @@ onMounted(() => {
   </ul>
 </template>
 
-<style scoped></style>
+<style scoped>
+.nested {
+  padding-left: 1rem;
+  list-style: none;
+}
+</style>
