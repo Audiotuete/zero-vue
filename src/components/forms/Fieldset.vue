1ccc7a49b66d30a786a65dfd774f42ad8adc73c0
Audiotuete <jan.moll@protonmail.com>
2023-10-26 16:08:55 +0000

    create: Fieldset component

---
+++ src/components/forms/Fieldset.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<Props>()
+interface Props {
+  legend: string
+}
+</script>
+
+<template>
+  <fieldset>
+    <legend>{{ props.legend }}</legend>
+    <slot />
+  </fieldset>
+</template>
+
+<style scoped>
+fieldset {
+  border: 1px solid black;
+  padding: 10px;
+  padding-top: 0;
+}
+</style>
